d8a43531d779ec39d93562be66ede1f76613db75
Janne Tuukkanen <janne@ibfekna.com>
2020-12-12 18:51:03 +0200

    add controller, model

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 require('dotenv').config()
 let MONGODB_URI=process.env.MONGODB_URI
 let PORT = process.env.PORT
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = 'mongodb://localhost/bloglist'
+// const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
@@ -29,23 +21,7 @@ mongoose.connect(MONGODB_URI, {
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
