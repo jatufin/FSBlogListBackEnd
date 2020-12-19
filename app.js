9bde1a93123ada610e3faa48c1311c3df1133679
Janne Tuukkanen <janne@ibfekna.com>
2020-12-20 01:34:01 +0200

    part 4.15

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -16,7 +18,8 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
